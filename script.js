dcbeb3fc682e4a576ed3acc460d3332800ee256e
SACHIN KUMAR PAL <101247442+sachin59921@users.noreply.github.com>
2024-09-16 10:40:01 +0530

    made changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = '04215c35d8ea8713f3eb911a13191fc4'; // Replace with your OpenWeatherMap API key
+const apiKey = '04215c35d8ea8713f3eb911a13191fc4';
 
 document.getElementById('getWeatherBtn').addEventListener('click', function() {
     const city = document.getElementById('cityInput').value;
